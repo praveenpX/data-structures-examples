3c8e5565c55da4cdcf1d6015eca31ad5576e10e2
prlabs <praveenpanati81@gmail.com>
2019-02-19 17:42:46 -0500

    Create PrintLargerLetterLexicoGraphicalOrder.cs

---
+++ PrintLargerLetterLexicoGraphicalOrder.cs
@@ -0,0 +1,39 @@
+using System; 
+public class Test 
+{ 
+   public static void Main() 
+   { 
+       //get length
+       int length = Convert.ToInt32(Console.ReadLine());
+       
+       //get string input
+       string input = Console.ReadLine();
+       
+       PrintLargerLetterLexicoGraphicalOrder(length, input);
+       
+   } 
+   
+   public static void PrintLargerLetterLexicoGraphicalOrder(int length, string input)
+   {
+       Char[] sChars = input.ToCharArray();
+       
+       int count = 0;
+       
+       for(int i =0; i <input.Length; i++)
+       {
+           count = 0;
+           
+           for(int j = 0; j <input.Length; j++)
+           {
+               int left = Convert.ToInt32(sChars[i]);
+               int right = Convert.ToInt32(sChars[j]);
+               
+               if(left < right)
+               {
+                   count++;
+               }
+           }
+           Console.Write(count + " ");
+       }
+   }
+}
